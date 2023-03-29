d434b0fabd0a8c2885160555623e10b840ea19e4
Sbansal97 <srijanb@andrew.cmu.edu>
2023-03-28 22:06:22 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ CUDA_VISIBLE_DEVICES=0,1 python test.py --data_file data_new.csv \
     --max_source_length 600 \
     --max_target_length 512 \
     --num_epochs 10 \
-    --batch_size 2 \
-    --grad_steps 2 \
+    --batch_size 8 \
+    --grad_steps 1 \
     --lr 5e-5
