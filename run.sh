1b83ced5b8df5fd3112223a1832c4fe2b9f2b118
Srijan_Bansal <srijanbansal97@gmail.com>
2023-03-30 20:06:02 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1 python test.py --data_file data_new.csv \
-    --model_name t5-large \
-    --instruction "Write a poem :" \
-    --max_source_length 600 \
-    --max_target_length 512 \
-    --num_epochs 10 \
-    --batch_size 8 \
-    --grad_steps 1 \
-    --lr 5e-5
